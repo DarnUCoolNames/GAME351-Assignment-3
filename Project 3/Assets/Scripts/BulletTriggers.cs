65730f8d1e2a987055420b1034744c0310176ccf
DarnUCoolNames <tntran@arizona.edu>
2023-04-25 19:16:56 -0700

    Added Audio Trigger

--- Project 3/Assets/Scripts/BulletTriggers.cs
+++ Project 3/Assets/Scripts/BulletTriggers.cs
@@ -4,35 +4,12 @@ using UnityEngine;
 
 public class BulletTriggers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-/*void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Barrel"))
         {
-            Vector3 position = other.transform.position;
-            Quaternion rotation = other.transform.rotation;
-
-            // Instantiate the replacement prefab at the same position and rotation
-            GameObject replacementObject = Instantiate(replacementPrefab, position, rotation);
-
             Destroy(other.gameObject);
-        }*/
-
-    void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Barrel"));
-            {
-                Destroy(other.gameObject);
-            }
         }
+    }
     }
