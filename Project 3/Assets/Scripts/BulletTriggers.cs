4335a473cd87c499fe798f2ed16856e145ec7707
IanRoach49 <ianroach@arizona.edu>
2023-04-27 23:01:52 -0700

    Exploding Barrels now functional

--- Project 3/Assets/Scripts/BulletTriggers.cs
+++ Project 3/Assets/Scripts/BulletTriggers.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class BulletTriggers : MonoBehaviour
 {
+    public GameObject prefab;
+    public ParticleSystem explosion;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Barrel"))
         {
             Destroy(other.gameObject);
+
+            ParticleSystem explosionEffect = Instantiate(explosion, other.transform.position, Quaternion.identity);
+            explosionEffect.Play();
+
+            GameObject brokenbarrel = Instantiate(prefab);
+            brokenbarrel.transform.position = other.gameObject.transform.position;
+            brokenbarrel.transform.rotation = other.gameObject.transform.rotation;
         }
     }
-    }
+    /*void OnCollisionEnter(Collision other)
+    {
+        if (collision.gameObject.CompareTag("Bandit"))
+        {
+            Destroy(other.gameObject);
+        }
+    }*/
+}
